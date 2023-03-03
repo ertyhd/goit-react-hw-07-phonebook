8603fef6b02dfbdd856606869f6ac697fcc7c332
ertyhd <orionship@protonmail.com>
2023-03-03 10:58:01 +0200

    end as lesson 16

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
 
-import { persistor, store } from 'redux/store';
+import { store } from './redux/store';
 import Contacts from 'components/Contacts';
 
 function App() {
   return (
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <Contacts />
-      </PersistGate>
+      <Contacts />
     </Provider>
   );
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,24 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import {
-  persistStore,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
 
 import rootReduser from './root-reducer';
 
 export const store = configureStore({
   reducer: rootReduser,
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
 });
 
-export const persistor = persistStore(store);
+// export default store;
